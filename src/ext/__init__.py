31cfa76abcb739bf7012c8a5484fe4cb56fcbd8b
Ryan <ryanoc1026@gmail.com>
2024-03-13 23:36:24 -0400

    fix __init__.py with module paths

--- src/ext/__init__.py
+++ src/ext/__init__.py
@@ -1,15 +1,8 @@
 import os
 
-MODULE_PATHS = []
-
-for root, _, _ in os.walk(".\\src\\ext"):
-    # pylint: disable=cell-var-from-loop
-    module_root = root.replace("\\", ".").strip(".")
-
-    module_names = filter(
-        lambda p: p.endswith(".py") and not p.startswith("_"), os.listdir(root)
-    )
-
-    modules_absolute = map(lambda p: f"{module_root}.{p[:-3]}", module_names)
-
-    MODULE_PATHS.extend(modules_absolute)
+MODULE_PATHS = [
+    os.path.join(dirpath, f).replace("\\", ".").strip(".")[:-3]
+    for (dirpath, _, filenames) in os.walk(".\\src\\ext")
+    for f in filenames
+    if f.endswith(".py") and not f.startswith("__")
+]
