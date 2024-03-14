2045708d7f503fab19117a239f0a9097f8e28b95
Ryan <ryanoc1026@gmail.com>
2024-03-13 23:39:20 -0400

    add sep

--- src/ext/__init__.py
+++ src/ext/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 MODULE_PATHS = [
-    os.path.join(dirpath, f).replace("\\", ".").strip(".")[:-3]
-    for (dirpath, _, filenames) in os.walk(".\\src\\ext")
+    os.path.join(dirpath, f).replace(os.sep, ".").strip(".")[:-3]
+    for (dirpath, _, filenames) in os.walk(f".{os.sep}src{os.sep}ext")
     for f in filenames
     if f.endswith(".py") and not f.startswith("__")
 ]
