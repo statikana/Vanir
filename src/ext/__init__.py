73f0f47366e5627d521142820981f965308990a0
Ryan <ryanoc1026@gmail.com>
2024-03-13 23:41:01 -0400

    no silent ignoring of os.walk

--- src/ext/__init__.py
+++ src/ext/__init__.py
@@ -1,8 +1,13 @@
+import logging
 import os
 
+    
 MODULE_PATHS = [
     os.path.join(dirpath, f).replace(os.sep, ".").strip(".")[:-3]
-    for (dirpath, _, filenames) in os.walk(f".{os.sep}src{os.sep}ext")
+    for (dirpath, _, filenames) in os.walk(
+        f".{os.sep}src{os.sep}ext",
+        onerror=lambda oserror: logging.fatal(oserror),
+    )
     for f in filenames
     if f.endswith(".py") and not f.startswith("__")
 ]
