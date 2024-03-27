e394da376e43738ac29e6a1ecf937cbbd81d81ee
statikana <86629697+statikana@users.noreply.github.com>
2024-03-26 22:33:31 -0400

    make reqs background

--- scripts/reqs.bat
+++ scripts/reqs.bat
@@ -1,2 +1,2 @@
-"c:/Users/ryan-ghost/Documents/GitHub/Vanir/.venv/Scripts/Activate.ps1"
+"& c:/Users/ryan-ghost/Documents/GitHub/Vanir/.venv/Scripts/Activate.ps1"
 pipreqs --ignore .venv,.vscode,.ruffcache,__pycache__ --encoding=utf-8 --force .
